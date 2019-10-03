4b9fd5adde89c9c97de16e9a9cbd4fea8beb46d8
lakospal0 <lakospal0@gmail.com>
2019-10-03 16:02:35 +0200

    todo list is done

--- main.ts
+++ main.ts
@@ -1,17 +1,30 @@
 'use strict';
 
-const fs=require('fs');
 
+const fs=require('fs');
+let printusage=  fs.readFileSync('./printusage.txt', 'utf-8');
 const args: string[] = process.argv;
 
 let mainProcess = () => {
 
   if (args.length == 2) {
-    console.log(" Let me help you\nCommand Line Todo application\n=============================\n\nCommand line arguments:\n\t-l Lists all the tasks\n\t-a   Adds a new task\n\t-r   Removes an task\n\t-c   Completes an task ");
-  } else if (args[2] == 'lis  t'){
-    console.log('i am listing all the todos')
-  } else {
-    console.log('this is the error handling');
+    console.log(printusage);
   }
 }
 mainProcess();
+
+//-----------------------------------------------------------------------------------
+
+let todolist=  fs.readFileSync('./todolist.txt', 'utf-8');
+
+todolist=todolist.split("\n");
+//console.log(todolist);
+
+if(args[2]=="-l"){
+for (let i=0;i<=todolist.length-1;i++){
+  console.log(i+1+" - "+todolist[i])
+}
+}
+  
+
+
