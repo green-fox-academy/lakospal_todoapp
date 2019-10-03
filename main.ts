ad407de7ca943ddd09f4c4a2bc98622ec3f57d0d
lakospal0 <lakospal0@gmail.com>
2019-10-03 16:49:27 +0200

    emptylist is done

--- main.ts
+++ main.ts
@@ -5,6 +5,7 @@ const fs=require('fs');
 let printusage=  fs.readFileSync('./printusage.txt', 'utf-8');
 const args: string[] = process.argv;
 
+//PRINT USAGE
 let mainProcess = () => {
 
   if (args.length == 2) {
@@ -14,17 +15,19 @@ let mainProcess = () => {
 mainProcess();
 
 //-----------------------------------------------------------------------------------
-
+//LIST TASKS && EMPTY LIST
 let todolist=  fs.readFileSync('./todolist.txt', 'utf-8');
 
 todolist=todolist.split("\n");
 //console.log(todolist);
 
-if(args[2]=="-l"){
-for (let i=0;i<=todolist.length-1;i++){
-  console.log(i+1+" - "+todolist[i])
+if(args[2]=="-l" && todolist.length==1 ){
+  console.log("no todo today ")
+}else if(args[2]=="-l"){
+  for (let i=0;i<=todolist.length-1;i++){
+  console.log(i+1+" - "+todolist[i])}
 }
-}
-  
+//------------------------------------------------------------------------------------------------
+
 
 
